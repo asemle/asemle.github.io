bcdb7a82db67a06e7a48748fa40c2fdc53990d68
asemle <asemle@umass.edu>
2017-02-13 16:42:28 -0500

    adding index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(__dirname + './'));
+
+module.exports = app;
+app.listen(port, function() {
+  console.log("Started server on port", port);
+});
