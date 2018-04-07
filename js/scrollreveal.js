6d52cf02701c06ec3a404b802768d175e7ae1dbe
asemle <asemle@umass.edu>
2018-04-07 15:10:29 -0600

    dsk

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -5,7 +5,7 @@ sr.reveal('#frontend', {origin: 'left', distance: '100vw'});
     sr.reveal('#responsive', { scale: 4, origin: 'bottom', distance: '50px', rotate: { x: 0, y: 0, z: 90}});
 sr.reveal('#backend', {origin: 'right', distance: '100vw'});
 
-    sr.reveal('.portfolio-item', { viewFactor: 0.5, duration: 800, rotate: { x: 0, y: 90, z: 0 }}, 100)
+    sr.reveal('.portfolio-item', { viewFactor: 0.5, duration: 800, rotate: { x: 0, y: 90, z: 0 }})
 
     sr.reveal('.skill', { viewFactor: 0.5, duration: 800, rotate: { x: 0, y: 0, z: 8}, opacity:0, scale:0.8, origin: 'bottom', distance:'50px' }, 30)
 
@@ -17,8 +17,6 @@ sr.reveal('#backend', {origin: 'right', distance: '100vw'});
     sr.reveal('.flipdown', {duration: 2000, distance: '0', rotate: { x: 90, y: 0, z: 0 }, scale:0.8 })
 
     sr.reveal('.flipup', { duration: 2000, distance: '0', rotate: { x: -90, y: 0, z: 0 }, scale: 0.8 })
-
-    sr.reveal('.social-buttons', { duration: 2000, distance: '0', scale:0.01})
 
     sr.reveal('.decolineleft', { opacity: 1, viewFactor: 0.5, duration: 3000, distance: '100vw', origin: 'left'}, 100)
 
