ac8f15399ca567ea2a0f97c89b4e264adc95dcae
asemle <asemle@umass.edu>
2020-11-17 16:51:40 -0500

    add bfinke and pmr

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -5,7 +5,7 @@ sr.reveal('#frontend', {origin: 'left', distance: '100vw'});
     sr.reveal('#responsive', { scale: 4, origin: 'bottom', distance: '50px', rotate: { x: 0, y: 0, z: 90}});
 sr.reveal('#backend', {origin: 'right', distance: '100vw'});
 
-    sr.reveal('.portfolio-item', { viewFactor: 0.5, duration: 800, rotate: { x: 0, y: 90, z: 0 }})
+    // sr.reveal('.portfolio-item', { viewFactor: 0.5, duration: 800, rotate: { x: 0, y: 90, z: 0 }})
 
     sr.reveal('.skill', { viewFactor: 0.5, duration: 800, rotate: { x: 0, y: 0, z: 8}, opacity:0, scale:0.8, origin: 'bottom', distance:'50px' }, 30)
 
